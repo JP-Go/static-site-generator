f02a0c7a88fd4622cc35ccf5724ce04fc37949d3
João Pedro <jpedro1580@gmail.com>
2024-05-22 23:21:21 -0300

    feat: create implementations to extract text nodes from md images and links

--- python/src/inline_md.py
+++ python/src/inline_md.py
@@ -1,5 +1,89 @@
-from typing import List
+import re
+from typing import Callable, List, Tuple, TypeVar
 from textnode import TextNode, TextType
+
+
+T = TypeVar("T")
+
+
+# INFO: This may be too much abstraction for only two cases of reuse. This
+# implementation is just showcasing a way to use function composition to
+# extract the list of nodes from a string and use recursion to aggregate them.
+def create_nodes_splitter(
+    extractor: Callable[[str], List[T]],
+    aggregator: Callable[[str, List[T]], List[TextNode]],
+):
+
+    def split_nodes(nodes: List[TextNode]) -> List[TextNode]:
+        new_nodes: List[TextNode] = []
+        for node in nodes:
+            if node.text_type != "text":
+                new_nodes.append(node)
+                continue
+            items = extractor(node.text)
+            has_items = len(items) > 0
+            if not has_items:
+                new_nodes.append(node)
+                continue
+            new_nodes.extend(aggregator(node.text, items))
+
+        return new_nodes
+
+    return split_nodes
+
+
+# INFO: Again, this may be too much abstraction for little reuse. This
+# implementation is just showcasing a way to use function composition
+# and recursion
+def create_aggregator(
+    sep_constructor: Callable[[T], str], node_constructor: Callable[[T], TextNode]
+):
+
+    def aggregate(text: str, aggregates: List[T]) -> List[TextNode]:
+        if text == "":
+            return []
+        if len(aggregates) == 0:
+            return [TextNode(text, "text")]
+        agg = aggregates[0]
+        sep = sep_constructor(agg)
+        [text_before, text_after] = text.split(sep, 1)
+
+        nodes = [TextNode(text_before, "text"), node_constructor(agg)]
+        nodes.extend(aggregate(text_after, aggregates[1:]))
+
+        return nodes
+
+    return aggregate
+
+
+def img_info_to_sep(img_info: Tuple[str, str]) -> str:
+    return f"![{img_info[0]}]({img_info[1]})"
+
+
+def img_info_to_text_node(img_info: Tuple[str, str]) -> TextNode:
+    return TextNode(img_info[0], "image", img_info[1])
+
+
+def link_info_to_sep(link_info: Tuple[str, str]) -> str:
+    return f"[{link_info[0]}]({link_info[1]})"
+
+
+def link_info_to_text_node(link_info: Tuple[str, str]) -> TextNode:
+    return TextNode(link_info[0], "link", link_info[1])
+
+
+aggregate_split_images = create_aggregator(img_info_to_sep, img_info_to_text_node)
+aggregate_split_links = create_aggregator(link_info_to_sep, link_info_to_text_node)
+
+
+def extract_markdown_images(text: str) -> List[Tuple[str, str]]:
+    pattern = re.compile(r"!\[(.+?)\]\((.+?)\)")
+    return re.findall(pattern, text)
+
+
+def extract_markdown_links(text: str) -> List[Tuple[str, str]]:
+    pattern = re.compile(r"(?<!\!)\[(.+?)\]\((.+?)\)")
+    return re.findall(pattern, text)
 
 
 def split_nodes_delimiter(
@@ -20,3 +104,9 @@ def split_nodes_delimiter(
                 [TextNode(text, "text") if i % 2 == 0 else TextNode(text, text_type)]
             )
     return new_nodes
+
+
+split_nodes_image = create_nodes_splitter(
+    extract_markdown_images, aggregate_split_images
+)
+split_nodes_link = create_nodes_splitter(extract_markdown_links, aggregate_split_links)
--- python/src/test_inline_md.py
+++ python/src/test_inline_md.py
@@ -1,5 +1,11 @@
 import unittest
-from inline_md import split_nodes_delimiter
+from inline_md import (
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_image,
+    split_nodes_link,
+)
 from textnode import TextNode
 
 
@@ -74,6 +80,257 @@ class TestSplitNodes(unittest.TestCase):
             "Invalid markdown syntax. Unclosed formatting delimiter",
             lambda: split_nodes_delimiter([node], "`", "code"),
         )
+
+
+class TestExtractImages(unittest.TestCase):
+    def test_extracts_one_image(self):
+        input = "Some image here: ![image](https://here.com)"
+        expected = [("image", "https://here.com")]
+        self.assertEqual(extract_markdown_images(input), expected)
+
+    def test_extracts_more_than_one_image(self):
+        input = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
+        expected = [
+            (
+                "image",
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png",
+            ),
+            (
+                "another",
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png",
+            ),
+        ]
+        self.assertEqual(extract_markdown_images(input), expected)
+
+    def test_extracts_no_image(self):
+        input = "There is no image in this"
+        expected = []
+        self.assertEqual(extract_markdown_images(input), expected)
+
+    def test_does_not_match_links(self):
+        input = (
+            "There is no image in this but there is a link [here](https://google.com)"
+        )
+        expected = []
+        self.assertEqual(extract_markdown_images(input), expected)
+
+
+class TestExtractLinks(unittest.TestCase):
+    def test_extracts_one_link(self):
+        input = "Some link here: [link](https://here.com)"
+        expected = [("link", "https://here.com")]
+        self.assertEqual(extract_markdown_links(input), expected)
+
+    def test_extracts_more_than_one_link(self):
+        input = "This is text with an [link](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and [another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
+        expected = [
+            (
+                "link",
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png",
+            ),
+            (
+                "another",
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png",
+            ),
+        ]
+        self.assertEqual(extract_markdown_links(input), expected)
+
+    def test_extracts_no_link(self):
+        input = "There is no link in this"
+        expected = []
+        self.assertEqual(extract_markdown_links(input), expected)
+
+    def test_does_not_extract_images(self):
+        input = "There is no link in this but theres an image ![here](https://some-image-here)"
+        expected = []
+        self.assertEqual(extract_markdown_links(input), expected)
+
+
+class TestSplitImages(unittest.TestCase):
+
+    def test_multiple_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another ![second image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+            "text",
+        )
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("This is text with an ", "text"),
+            TextNode(
+                "image",
+                "image",
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png",
+            ),
+            TextNode(" and another ", "text"),
+            TextNode(
+                "second image",
+                "image",
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png",
+            ),
+        ]
+        self.assertEqual(expected, new_nodes)
+
+    def test_one_image(self):
+        node = TextNode(
+            (
+                "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)"
+                " and no other image"
+            ),
+            "text",
+        )
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("This is text with an ", "text"),
+            TextNode(
+                "image",
+                "image",
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png",
+            ),
+            TextNode(" and no other image", "text"),
+        ]
+        self.assertEqual(expected, new_nodes)
+
+    def test_one_image_and_link(self):
+        node = TextNode(
+            (
+                "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)"
+                " and no other image but a link [link](https://google.com)"
+            ),
+            "text",
+        )
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode("This is text with an ", "text"),
+            TextNode(
+                "image",
+                "image",
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png",
+            ),
+            TextNode(
+                " and no other image but a link [link](https://google.com)", "text"
+            ),
+        ]
+        self.assertEqual(expected, new_nodes)
+
+    def test_no_image_and_link(self):
+        node = TextNode(
+            ("This is text with no image" " and a link [link](https://google.com)"),
+            "text",
+        )
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode(
+                "This is text with no image and a link [link](https://google.com)",
+                "text",
+            ),
+        ]
+        self.assertEqual(expected, new_nodes)
+
+    def test_just_text(self):
+        node = TextNode(
+            ("This is text with no image and no links"),
+            "text",
+        )
+        new_nodes = split_nodes_image([node])
+        expected = [
+            TextNode(
+                "This is text with no image and no links",
+                "text",
+            ),
+        ]
+        self.assertEqual(expected, new_nodes)
+
+
+class TestSplitLinks(unittest.TestCase):
+    def test_multiple_links(self):
+        node = TextNode(
+            "This is text with an [link](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and another [second link](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png)",
+            "text",
+        )
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("This is text with an ", "text"),
+            TextNode(
+                "link",
+                "link",
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png",
+            ),
+            TextNode(" and another ", "text"),
+            TextNode(
+                "second link",
+                "link",
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/3elNhQu.png",
+            ),
+        ]
+        self.assertEqual(expected, new_nodes)
+
+    def test_one_link(self):
+        node = TextNode(
+            (
+                "This is text with an [link](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)"
+                " and no other link"
+            ),
+            "text",
+        )
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("This is text with an ", "text"),
+            TextNode(
+                "link",
+                "link",
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png",
+            ),
+            TextNode(" and no other link", "text"),
+        ]
+        self.assertEqual(expected, new_nodes)
+
+    def test_one_image_and_link(self):
+        node = TextNode(
+            (
+                "This is text with an [link1](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)"
+                " and an image ![link2](https://google.com)"
+            ),
+            "text",
+        )
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode("This is text with an ", "text"),
+            TextNode(
+                "link1",
+                "link",
+                "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png",
+            ),
+            TextNode(" and an image ![link2](https://google.com)", "text"),
+        ]
+        self.assertEqual(expected, new_nodes)
+
+    def test_no_link_and_an_image(self):
+        node = TextNode(
+            ("This is text with no link and an image ![like this](https://google.com)"),
+            "text",
+        )
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode(
+                "This is text with no link and an image ![like this](https://google.com)",
+                "text",
+            ),
+        ]
+        self.assertEqual(expected, new_nodes)
+
+    def test_just_text(self):
+        node = TextNode(
+            "This is text with no image and no links",
+            "text",
+        )
+        new_nodes = split_nodes_link([node])
+        expected = [
+            TextNode(
+                "This is text with no image and no links",
+                "text",
+            ),
+        ]
+        self.assertEqual(expected, new_nodes)
 
 
 if __name__ == "__main__":
